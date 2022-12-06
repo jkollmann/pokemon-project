aa387a9f2b93b1f4b799595f9f5268660f179a63
SuperCoder <supercoder@SuperCoders-MacBook-Air.local>
2022-12-06 09:57:20 +0100

    Use the new navigation component

--- src/pages/productlist/ProductList.js
+++ src/pages/productlist/ProductList.js
@@ -1,8 +1,10 @@
+import Navigation from "../../components/navigation/Navigation";
 import ProductDetail from "../producdetail/ProductDetail";
 
 function ProductList() {
     return (
         <section>
+            <Navigation />
             <p>List of Products</p>
             <ProductDetail />
             <ProductDetail />
